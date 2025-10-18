946537f20d85948e646b12774d12409bc381ecc0
kevdunkel <kdunkel@purdue.edu>
2025-10-18 15:14:33 -0400

    Update chassis_task.c

--- Minibot/src/chassis_task.c
+++ Minibot/src/chassis_task.c
@@ -1,5 +1,5 @@
 #include "chassis_task.h"
-
+#include <math.h>
 #include "robot.h"
 #include "remote.h"
 #include "motor.h"
@@ -8,7 +8,12 @@
 extern Robot_State_t g_robot_state;
 extern Remote_t g_remote;
 
-float chassis_rad;
+float chassis_rad = 5.0; // radius of the chassis wheels
+DJI_Motor_Handle_t* motor_w1;
+DJI_Motor_Handle_t* motor_w2;
+DJI_Motor_Handle_t* motor_w3;
+DJI_Motor_Handle_t* motor_w4;
+
 
 void Chassis_Task_Init()
 {
@@ -81,4 +86,20 @@ void Chassis_Task_Init()
 void Chassis_Ctrl_Loop()
 {
     // Control loop for the chassis
+
+    float d = 15.0;        
+    float rad = PI/4;
+    float omega = 1.0;
+    float dx = g_robot_state.input.vx;    
+    float dy = g_robot_state.input.vy;
+
+    float Phi1 = -sin(rad)*dx + cos(rad)*dy + omega*d;
+    float Phi2 = -cos(rad)*dx - sin(rad)*dy + omega*d;
+    float Phi3 =  sin(rad)*dx - cos(rad)*dy + omega*d;
+    float Phi4 =  cos(rad)*dx + sin(rad)*dy + omega*d;
+
+    DJI_Motor_Set_Velocity(motor_w1, Phi1*(1/chassis_rad));
+    DJI_Motor_Set_Velocity(motor_w2, Phi2*(1/chassis_rad));
+    DJI_Motor_Set_Velocity(motor_w3, Phi3*(1/chassis_rad));
+    DJI_Motor_Set_Velocity(motor_w4, Phi4*(1/chassis_rad));
 }
